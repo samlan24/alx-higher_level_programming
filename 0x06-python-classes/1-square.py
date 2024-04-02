f6e4ec7af204bf481adeb2c64f0cb7a8c2639f7e
samlan24 <muchemionline@gmail.com>
2024-04-02 11:19:00 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """A square representation"""
+
+
+def __int__(self, size):
+    """instance of a square class
+    Args: size - size of a square
+    """
+
+    self.__size = size
