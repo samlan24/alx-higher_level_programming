a5eb47d5906c39c3bd45a67655389e8fe53acdb1
samlan24 <muchemionline@gmail.com>
2024-04-23 06:54:30 -0700

    updates

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""models for square class"""
+"""This module contains a square class"""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """square class"""
-
+    """Represents a square"""
     def __init__(self, size, x=0, y=0, id=None):
         self.size = size
         self.x = x
         self.y = y
         self.id = None
-        super.__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """format for string representation of the square class"""
-        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
+        """Defines a format for the string representation of the class"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
