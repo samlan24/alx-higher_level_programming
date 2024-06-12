e29e16399aa848f276116995ae5d52a10804442e
samlan24 <muchemionline@gmail.com>
2024-06-12 03:54:31 -0700

    3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,3 @@
 --creates a table force_name
 
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
