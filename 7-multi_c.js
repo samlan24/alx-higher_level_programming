a71beb67b1bd31f6ccb5074b401ce1e257ce9d77
samlan24 <muchemionline@gmail.com>
2024-06-17 06:07:19 -0700

    7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv[2];
+const num = parseInt(args);
+
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
+
