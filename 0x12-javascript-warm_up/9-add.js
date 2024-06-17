5fe1ec6ec3eb023d56620c9186b68ed0af52bfbb
samlan24 <muchemionline@gmail.com>
2024-06-17 10:17:09 -0700

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
+const a = process.argv[2];
+const b = process.argv[3];
 
 function add (a, b) {
   return (a + b);
 }
 
-console.log(add(arg1, arg2));
+console.log(add(parseInt(a), parseInt(b)));
