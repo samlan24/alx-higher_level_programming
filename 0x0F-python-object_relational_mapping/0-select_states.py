617d2b31f8ea830795eada7dd279f741d91ab189
samlan24 <muchemionline@gmail.com>
2024-06-22 00:13:19 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
+
 import MySQLdb
 from sys import argv
 
-# Connect to the database
-db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
-# Create a cursor object to interact with the database
-cursor = db.cursor()
+    db_cursor = db_connect.cursor()
 
-# Example table name
-table_name = 'states'
+    db_cursor.execute("SELECT * FROM states")
 
-# Example 1: Select data from the table
-select_query = f"SELECT * FROM {table_name}"
-cursor.execute(select_query)
+    rows_selected = db_cursor.fetchall()
 
-# Fetch all the rows from the executed query
-rows = cursor.fetchall()
-
-# Print the fetched rows (data)
-for row in rows:
-    print(row)
-
-# Close the cursor and the connection
-cursor.close()
-db.close()
+    for row in rows_selected:
+        print(row)
