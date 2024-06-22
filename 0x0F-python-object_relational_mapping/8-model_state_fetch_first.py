ea16adb7b9ff24de99e05458c9b672a6505b4d82
samlan24 <muchemionline@gmail.com>
2024-06-22 06:12:36 -0700

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -25,4 +25,6 @@ if __name__ == "__main__":
     first_state = session.query(State).order_by(State.id).first()
     if first_state:
         print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
     session.close()
