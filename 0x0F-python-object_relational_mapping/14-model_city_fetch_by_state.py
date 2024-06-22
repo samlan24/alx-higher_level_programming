2e61f9837b3d8d817754a50ced4a873671f1bad4
samlan24 <muchemionline@gmail.com>
2024-06-22 09:23:27 -0700

    updating files

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-
 """
-prints all City objects
-from the database hbtn_0e_14_usa
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
 """
 
+from sys import argv
+from model_state import State, Base
+from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
-from model_state import Base, State
-from model_city import City
 
 if __name__ == "__main__":
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
-    # creating engine
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
+
     engine = create_engine(db_url)
-
-    # a sessionmaker to manage sessions
     Session = sessionmaker(bind=engine)
-    # this is a session object
-    session = Session()
-    Base.metadata.create_all(engine)
 
-    # printing city, id, and state
+    session = Session()
+
     results = session.query(City, State).join(State)
 
     for city, state in results.all():
-        print(f"{state.name}: ({city.id}) {city.name}")
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.commit()
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """
-Python file that contains the class definition of a city and an instance
+This script defines a City class
+to work with MySQLAlchemy ORM.
 """
+from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
 
 
 class City(Base):
-    """
-    State class that inherits from Base
-
+    """City class
     Attributes:
-        id: Id city
-        name: Name of city
+        __tablename__ (str): The table name of the class
+        id (int): The id of the class
+        name (str): The name of the class
+        state_id (int): The state the city belongs to
     """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
