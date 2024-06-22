bd6136f7c1f7caf718b70324c076c3129340e84e
samlan24 <muchemionline@gmail.com>
2024-06-22 01:29:26 -0700

    4-cities_by_state.

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    printing the argument
+    """
+
+    db_user = argv[1]
+    db_passwd = argv[2]
+    db_name = argv[3]
+
+    db_connect = MySQLdb.connect(
+        host="localhost",
+        user=db_user,
+        port=3306,
+        passwd=db_passwd,
+        db=db_name
+    )
+
+    db_cursor = db_connect.cursor()
+
+    query = (
+        "SELECT * FROM cities "
+    )
+
+    db_cursor.execute(query,)
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
+
+    db_cursor.close()
+    db_connect.close()
