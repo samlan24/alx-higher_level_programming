297ed126b08799357fa959b49da336273d35c94f
samlan24 <muchemionline@gmail.com>
2024-07-13 06:13:11 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/env bash
-# returns the size of the body in bytes
-
-if [ -z "$1" ]; then
-   exit 1
-fi
-
-URL=$1
-SIZE=$(curl -s -w '%{size_download}\n' -o /dev/null "$URL")
-echo "$SIZE"
+#!/bin/bash
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
