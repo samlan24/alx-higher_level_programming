d01f3ec8af1ee362c2ed5cbd50b11e08db3a8d5a
samlan24 <muchemionline@gmail.com>
2024-08-03 07:38:29 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 /*
-script that writes code status  
+script that writes code status
 */
 
-const { STATUS_CODES } = require('http');
 const request = require('request');
 const URL = process.argv[2];
 
